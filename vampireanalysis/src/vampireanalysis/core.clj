1d2825a1a21d45b63221a4b171e8e98d66cbb193
Eduardo Pitz <pitz.eduardo@gmail.com>
2021-11-20 12:16:06 -0300

    Criando power-index-filter!

--- vampireanalysis/src/vampireanalysis/core.clj
+++ vampireanalysis/src/vampireanalysis/core.clj
@@ -1,7 +1,10 @@
 (ns vampireanalysis.core)
+
 (def filename "suspects.csv")
 
-(def vampire-keys [:name :power-index])
+(def vampire-keys 
+    [:name 
+    :power-index])
 
 (defn str->int
     [str]
@@ -11,12 +14,36 @@
                   :power-index str->int})
 
 (defn convert
-      [vamp-key value]
-      ((get conversions vamp-key) value))
+      [vampire-key value]
+      ((get conversions vampire-key) value))
 
 (defn parse
     [string]
     (map #(clojure.string/split % #",")
           (clojure.string/split string #"\n")))
 
-(println (parse (slurp filename)))
+;; (println (parse (slurp filename)))
+
+(defn mapify
+    [rows]
+    (map (fn [unmapped-row]
+        (reduce (fn [row-map [vampire-key value]]
+                    (assoc row-map vampire-key (convert vampire-key value)))
+                {}
+                (map vector vampire-keys unmapped-row)))
+        rows))
+
+;; Código comentado é feio mesmo mas irei manter isso por aqui.
+;; (println "Mapear todos: ")
+;; (println (mapify (parse (slurp filename))))
+;; (println "Take 1: ")
+;; (println (take 1 (mapify (parse (slurp filename)))))
+;; (println "Ordenar pelo :name: ")
+;; (println (sort-by :name (mapify (parse (slurp filename)))))
+
+(defn power-index-filter
+    [minimum-power-index records]
+    (filter #(>= (:power-index %) minimum-power-index) records))
+
+(println "VAMPIROS DETECTADOS: ")
+(println (power-index-filter 3 (mapify (parse (slurp filename)))))
